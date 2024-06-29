247bd8c754b135dace7ba83458d02c3228a37636
runningwater <ynwdlxm@163.com>
2024-06-30 02:25:14 +0800

    support generate csv to json and yaml

--- src/process.rs
+++ src/process.rs
@@ -2,6 +2,9 @@ use std::fs;
 
 use csv::Reader;
 use serde::{Deserialize, Serialize};
+use serde_json::Value;
+
+use crate::opts::OutputFormat;
 
 #[derive(Debug, Serialize, Deserialize)]
 struct Player {
@@ -17,15 +20,22 @@ struct Player {
     kit: u8,
 }
 
-pub fn process_csv(input: &str, output: &str) -> anyhow::Result<()> {
+pub fn process_csv(input: &str, output: String, format: OutputFormat) -> anyhow::Result<()> {
     let mut reader = Reader::from_path(input)?;
     let mut ret = Vec::with_capacity(128);
-    for result in reader.deserialize() {
-        let record: Player = result?;
-        ret.push(record);
+    let headers = reader.headers()?.clone();
+    for result in reader.records() {
+        let record = result?;
+        let json_value = headers.iter().zip(record.iter()).collect::<Value>();
+        ret.push(json_value);
     }
-    let json = serde_json::to_string_pretty(&ret)?;
-    fs::write(output, json)?;
+
+    let contents = match format {
+        OutputFormat::Json => serde_json::to_string_pretty(&ret)?,
+        OutputFormat::Yaml => serde_yml::to_string(&ret)?,
+    };
+
+    fs::write(output, contents)?;
 
     Ok(())
 }
